7848089aeb7356e518235294abae4bfe8c324b43
Alessandro Schutz <alessandro231.schutz@hotmail.com>
2024-09-05 09:29:50 -0300

    estudos typescript

--- TypeScript/3_Avancando_em_tipos/index.ts
+++ TypeScript/3_Avancando_em_tipos/index.ts
@@ -22,3 +22,36 @@ const arr1: any = [1, "teste", true, [], { nome: "Mateus" }];
 
 arr1.push([1, 2, 3, 4, 5]);
 console.log(arr1, typeof arr1);
+
+// 4 - parametros tipados
+function soma(a: number, b: number) {
+  console.log(a + b);
+}
+
+soma(9, 8);
+
+// 5 - Retorno de função
+function greeting(name: string): string {
+  return `Olá ${name}`;
+}
+
+console.log(greeting("Mateus"));
+
+// 6 - Função anonima
+setTimeout(function () {
+  const sallary: number = 1000;
+
+  // console.log(parseFloat(sallary));
+
+  // console.log(sallary);
+}, 2000);
+
+// 7 - Tipos de objetos
+
+function passCoordinates(coord: { x: number; y: number }) {
+  console.log("X coordinates: " + coord.x);
+  console.log("Y coordinates: " + coord.y);
+}
+
+const objCoord = { x: 329, y: 84.2 };
+passCoordinates(objCoord);
